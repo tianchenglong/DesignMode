36eaea6fca9ad7199c57b10534effa327c1c84e3
tiancl <chenglong.tian@baifendian.com>
2018-11-03 15:04:45 +0800

    初始化

---
+++ src/main/java/com/tcl/designmode/DesignmodeApplication.java
@@ -0,0 +1,12 @@
+package com.tcl.designmode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignmodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignmodeApplication.class, args);
+	}
+}
