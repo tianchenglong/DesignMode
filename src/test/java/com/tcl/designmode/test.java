0a27d35468d7f980009211b0661b195aa2016924
tianchenglong <hland123>
2019-01-05 22:53:19 +0800

    测试

---
+++ src/test/java/com/tcl/designmode/test.java
@@ -0,0 +1,28 @@
+package com.tcl.designmode;
+
+/**
+ * @author: chenglong.tian@foxmail.com
+ * <p>
+ * create: 2018-12-17 22:56
+ * @version: 1.0
+ **/
+public class test {
+    public static void main(String [] args){
+        Integer a =1;
+        Integer b =2;
+        Integer c =3;
+        Integer d = 3;
+        Integer e  =321;
+        Integer f   =321;
+        Long g =3l;
+        System.out.println(c ==d);
+        System.out.println(e== f);
+        System.out.println(c ==(a+b));
+        System.out.println(c.equals(a+b));
+        System.out.println(g==(a+b));
+        System.out.println(g.equals(a+b));
+
+
+
+    }
+}
